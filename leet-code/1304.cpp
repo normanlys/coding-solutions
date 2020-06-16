04a00c494a725ef1dec0f2152c61e2eee3d20f1c
Norman Lim <normanlys16@gmail.com>
2020-06-17 01:41:39 +0800

    leetcode 1304

---
+++ leet-code/1304.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) 
+    {
+        vector<int> result{};
+        if (n%2 == 1)
+        {
+            result.push_back(0);
+            n--;
+        }
+
+        while (n > 0)
+        {
+            result.push_back(n);
+            result.push_back(-n);
+            n -= 2;
+        }
+        return result;
+    }
+};
