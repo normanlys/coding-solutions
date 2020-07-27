f332450a8bde9e770718aee3cbad8721e5113b51
Norman Lim <normanlys16@gmail.com>
2020-07-27 11:42:25 +0800

    Add week4

---
+++ coursera-algorithms/week4/kargerMinCut.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+    ifstream file("kargerMinCut.txt");
+    string line;
+    while (file >> line)
+    {
+        
+    }
+    return 0;
+}
